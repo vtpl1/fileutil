471869d5c8b53edc20981a04825bf46dc2fd11eb
Monotosh Das <monotosh.das@videonetics.com>
2023-03-07 00:53:40 +0530

    file utilities export added

--- include/file_utilities.h
+++ include/file_utilities.h
@@ -10,27 +10,31 @@
 #include <string>
 #include <tuple>
 #include <vector>
+
+#include <fileutil_export.h>
+#include <version.h>
+
 namespace vtpl
 {
 namespace utilities
 {
-std::string get_filesystem_directory_seperator();
-bool is_directory_exists(const std::string& dir_path);
-bool is_regular_file_exists(const std::string& file_path);
-bool create_directories(const std::string& dir_path);
-std::string create_directories_from_file_path(const std::string& file_path);
-std::stringstream end_with_directory_seperator(const std::string& dir_path_0);
-std::string merge_directories(const std::string& dir_path_0, const std::string& dir_path_1);
-std::string merge_directories(const std::string& dir_path_0, const std::string& dir_path_1,
-                              const std::string& dir_path_2);
-std::string merge_directories(const std::string& dir_path_0, const std::string& dir_path_1,
-                              const std::string& dir_path_2, const std::string& dir_path_3);
-std::string merge_directories(const std::string& dir_path_0, const std::string& dir_path_1,
-                              const std::string& dir_path_2, const std::string& dir_path_3,
-                              const std::string& dir_path_4);
-std::string merge_directories(const std::string& dir_path, std::vector<std::string>& dir_n_list);
-bool delete_directory(const std::string& dir_path);
-bool delete_file(const std::string& file_path);
+std::string FILEUTIL_EXPORT get_filesystem_directory_seperator();
+bool FILEUTIL_EXPORT is_directory_exists(const std::string& dir_path);
+bool FILEUTIL_EXPORT is_regular_file_exists(const std::string& file_path);
+bool FILEUTIL_EXPORT create_directories(const std::string& dir_path);
+std::string FILEUTIL_EXPORT create_directories_from_file_path(const std::string& file_path);
+std::stringstream FILEUTIL_EXPORT end_with_directory_seperator(const std::string& dir_path_0);
+std::string FILEUTIL_EXPORT merge_directories(const std::string& dir_path_0, const std::string& dir_path_1);
+std::string FILEUTIL_EXPORT merge_directories(const std::string& dir_path_0, const std::string& dir_path_1,
+                                              const std::string& dir_path_2);
+std::string FILEUTIL_EXPORT merge_directories(const std::string& dir_path_0, const std::string& dir_path_1,
+                                              const std::string& dir_path_2, const std::string& dir_path_3);
+std::string FILEUTIL_EXPORT merge_directories(const std::string& dir_path_0, const std::string& dir_path_1,
+                                              const std::string& dir_path_2, const std::string& dir_path_3,
+                                              const std::string& dir_path_4);
+std::string FILEUTIL_EXPORT merge_directories(const std::string& dir_path, std::vector<std::string>& dir_n_list);
+bool FILEUTIL_EXPORT delete_directory(const std::string& dir_path);
+bool FILEUTIL_EXPORT delete_file(const std::string& file_path);
 } // namespace utilities
 } // namespace vtpl
 #endif // file_utilities_h
