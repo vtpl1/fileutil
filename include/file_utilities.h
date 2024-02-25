0d32e6265b050ee44b79c441efcf182aff036382
Monotosh Das <monotosh.das@videonetics.com>
2024-02-25 19:52:35 +0000

    std stream removed

--- include/file_utilities.h
+++ include/file_utilities.h
@@ -5,25 +5,21 @@
 #pragma once
 #ifndef file_utilities_h
 #define file_utilities_h
+#include <fileutil_export.h>
 #include <map>
-#include <sstream>
 #include <string>
 #include <tuple>
 #include <vector>
-
-#include <fileutil_export.h>
 #include <version.h>
 
-namespace vtpl
-{
-namespace utilities
-{
+namespace vtpl {
+namespace utilities {
 std::string FILEUTIL_EXPORT get_filesystem_directory_seperator();
-bool FILEUTIL_EXPORT is_directory_exists(const std::string& dir_path);
-bool FILEUTIL_EXPORT is_regular_file_exists(const std::string& file_path);
-bool FILEUTIL_EXPORT create_directories(const std::string& dir_path);
+bool FILEUTIL_EXPORT        is_directory_exists(const std::string& dir_path);
+bool FILEUTIL_EXPORT        is_regular_file_exists(const std::string& file_path);
+bool FILEUTIL_EXPORT        create_directories(const std::string& dir_path);
 std::string FILEUTIL_EXPORT create_directories_from_file_path(const std::string& file_path);
-std::stringstream FILEUTIL_EXPORT end_with_directory_seperator(const std::string& dir_path_0);
+std::string FILEUTIL_EXPORT end_with_directory_seperator(const std::string& dir_path_0);
 std::string FILEUTIL_EXPORT merge_directories(const std::string& dir_path_0, const std::string& dir_path_1);
 std::string FILEUTIL_EXPORT merge_directories(const std::string& dir_path_0, const std::string& dir_path_1,
                                               const std::string& dir_path_2);
@@ -33,8 +29,8 @@ std::string FILEUTIL_EXPORT merge_directories(const std::string& dir_path_0, con
                                               const std::string& dir_path_2, const std::string& dir_path_3,
                                               const std::string& dir_path_4);
 std::string FILEUTIL_EXPORT merge_directories(const std::string& dir_path, std::vector<std::string>& dir_n_list);
-bool FILEUTIL_EXPORT delete_directory(const std::string& dir_path);
-bool FILEUTIL_EXPORT delete_file(const std::string& file_path);
+bool FILEUTIL_EXPORT        delete_directory(const std::string& dir_path);
+bool FILEUTIL_EXPORT        delete_file(const std::string& file_path);
 } // namespace utilities
 } // namespace vtpl
 #endif // file_utilities_h
--- src/file_utilities.cpp
+++ src/file_utilities.cpp
@@ -1,9 +1,14 @@
 // *****************************************************
 //    Copyright 2023 Videonetics Technology Pvt Ltd
 // *****************************************************
-#include <iostream>
-
 #include "file_utilities.h"
+#include <cstddef>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <system_error>
+#include <vector>
 
 #ifdef WIN32
 #include <filesystem>
@@ -20,8 +25,7 @@ namespace filesystem = std::experimental::filesystem;
 #endif
 #endif
 
-std::string vtpl::utilities::get_filesystem_directory_seperator()
-{
+std::string vtpl::utilities::get_filesystem_directory_seperator() {
 #ifdef _WIN32
   return "\\";
 #else
@@ -30,24 +34,21 @@ std::string vtpl::utilities::get_filesystem_directory_seperator()
   // return "/";
 }
 
-bool vtpl::utilities::is_directory_exists(const std::string& dir_path)
-{
+bool vtpl::utilities::is_directory_exists(const std::string& dir_path) {
   if (dir_path.empty()) {
     throw std::runtime_error("Input argument is empty");
   }
   return (filesystem::exists(dir_path) && filesystem::is_directory(dir_path));
 }
 
-bool vtpl::utilities::is_regular_file_exists(const std::string& file_path)
-{
+bool vtpl::utilities::is_regular_file_exists(const std::string& file_path) {
   if (file_path.empty()) {
     throw std::runtime_error("Input argument is empty");
   }
   return (filesystem::exists(file_path) && filesystem::is_regular_file(file_path));
 }
 
-bool vtpl::utilities::create_directories(const std::string& dir_path)
-{
+bool vtpl::utilities::create_directories(const std::string& dir_path) {
   if (dir_path.empty()) {
     throw std::runtime_error("Input argument is empty");
   }
@@ -59,19 +60,18 @@ bool vtpl::utilities::create_directories(const std::string& dir_path)
     std::cout << "Unable to create directories [" << dir_path << "], "
               << "Error Code[" << error_code.value() << "], "
               << "Error Message[" << error_code.message() << "], "
-              << "Error category[" << error_code.category().name() << "] " << std::endl;
+              << "Error category[" << error_code.category().name() << "] " << '\n';
     return false;
   }
   return vtpl::utilities::is_directory_exists(dir_path);
 }
 
-std::string vtpl::utilities::create_directories_from_file_path(const std::string& file_path)
-{
+std::string vtpl::utilities::create_directories_from_file_path(const std::string& file_path) {
   if (file_path.empty()) {
     throw std::runtime_error("Input argument is empty");
   }
-  std::string dir_path;
-  size_t pos = file_path.find_last_of(get_filesystem_directory_seperator());
+  std::string  dir_path;
+  const size_t pos = file_path.find_last_of(get_filesystem_directory_seperator());
   if (pos != std::string::npos) {
     dir_path = file_path.substr(0, pos);
   }
@@ -85,24 +85,23 @@ std::string vtpl::utilities::create_directories_from_file_path(const std::string
   return std::string{};
 }
 
-std::stringstream vtpl::utilities::end_with_directory_seperator(const std::string& dir_path_0)
-{
+std::string vtpl::utilities::end_with_directory_seperator(const std::string& dir_path_0) {
   std::stringstream ss_path;
   ss_path << dir_path_0;
-  if (!((dir_path_0.back() == '/') || (dir_path_0.back() == '\\'))) {
+  if ((dir_path_0.back() != '/') && (dir_path_0.back() != '\\')) {
     ss_path << get_filesystem_directory_seperator();
   }
-  return ss_path;
+  return ss_path.str();
 }
 
-std::string vtpl::utilities::merge_directories(const std::string& dir_path_0, const std::string& dir_path_1)
-{
+std::string vtpl::utilities::merge_directories(const std::string& dir_path_0, const std::string& dir_path_1) {
   std::stringstream ss_path;
   if ((dir_path_0.empty()) || (dir_path_1.empty())) {
     throw std::runtime_error("Input argument is empty");
   }
   ss_path << dir_path_0;
-  std::stringstream ss_path1 = end_with_directory_seperator(ss_path.str());
+  std::stringstream ss_path1;
+  ss_path1 << end_with_directory_seperator(ss_path.str());
   ss_path.swap(ss_path1);
   ss_path << dir_path_1;
   // if (!((ss_path.str().back() == '/') || (ss_path.str().back() == '\\')))
@@ -111,38 +110,33 @@ std::string vtpl::utilities::merge_directories(const std::string& dir_path_0, co
 }
 
 std::string vtpl::utilities::merge_directories(const std::string& dir_path_0, const std::string& dir_path_1,
-                                               const std::string& dir_path_2)
-{
+                                               const std::string& dir_path_2) {
   // NOLINTNEXTLINE(readability-suspicious-call-argument)
   return merge_directories(dir_path_0, merge_directories(dir_path_1, dir_path_2));
 }
 
 std::string vtpl::utilities::merge_directories(const std::string& dir_path_0, const std::string& dir_path_1,
-                                               const std::string& dir_path_2, const std::string& dir_path_3)
-{
+                                               const std::string& dir_path_2, const std::string& dir_path_3) {
   // NOLINTNEXTLINE(readability-suspicious-call-argument)
   return merge_directories(dir_path_0, merge_directories(dir_path_1, dir_path_2, dir_path_3));
 }
 
 std::string vtpl::utilities::merge_directories(const std::string& dir_path_0, const std::string& dir_path_1,
                                                const std::string& dir_path_2, const std::string& dir_path_3,
-                                               const std::string& dir_path_4)
-{
+                                               const std::string& dir_path_4) {
   // NOLINTNEXTLINE(readability-suspicious-call-argument)
   return merge_directories(dir_path_0, merge_directories(dir_path_1, dir_path_2, dir_path_3, dir_path_4));
 }
 
-std::string vtpl::utilities::merge_directories(const std::string& dir_path, std::vector<std::string>& dir_n_list)
-{
+std::string vtpl::utilities::merge_directories(const std::string& dir_path, std::vector<std::string>& dir_n_list) {
   std::string path(dir_path);
-  for (auto& it : dir_n_list) {
-    path = merge_directories(path, it);
+  for (auto& dir : dir_n_list) {
+    path = merge_directories(path, dir);
   }
   return path;
 }
 
-bool vtpl::utilities::delete_directory(const std::string& dir_path)
-{
+bool vtpl::utilities::delete_directory(const std::string& dir_path) {
   if (dir_path.empty()) {
     throw std::runtime_error("Input argument is empty");
   }
@@ -152,8 +146,7 @@ bool vtpl::utilities::delete_directory(const std::string& dir_path)
   return true;
 }
 
-bool vtpl::utilities::delete_file(const std::string& file_path)
-{
+bool vtpl::utilities::delete_file(const std::string& file_path) {
   if (file_path.empty()) {
     throw std::runtime_error("Input argument is empty");
   }
